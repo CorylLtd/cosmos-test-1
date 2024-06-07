44f3dcb9f540c21358846a0d0f35e06b3fecbb7b
Martin Randall <martin@coryl.com>
2024-06-07 23:35:03 +0100

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,38 @@
 'use client';
 
 import { getFamily } from '@/actions/db';
+import { Family } from '@/types/family';
+import { useState } from 'react';
 
 export default async function Home() {
-  const family = await getFamily('Randall');
+  const [family, setFamily] = useState<Family | null>(null);
+
   return (
     <main className="p-4">
       <h1 className="text-3xl font-bold mb-4">Home</h1>
-      <p className="mb-3">
-        Welcome, {family.parents[0].firstName} {family.parents[0].familyName}!
-      </p>
-      <p className="mb-3">
-        You are married to {family.parents[1].firstName}{' '}
-        {family.parents[1].familyName}.
-      </p>
-      <p className="mb-3">
-        You have {family.children.length} children:{' '}
-        {family.children.map((child) => (
-          <span key={child.firstName}>
-            {child.firstName} {child.familyName},{' '}
-          </span>
-        ))}
-      </p>
+      <button onClick={async () => setFamily(await getFamily('Randall'))}>
+        Get Family
+      </button>
+      {family && (
+        <>
+          <p className="mb-3">
+            Welcome, {family.parents[0].firstName}{' '}
+            {family.parents[0].familyName}!
+          </p>
+          <p className="mb-3">
+            You are married to {family.parents[1].firstName}{' '}
+            {family.parents[1].familyName}.
+          </p>
+          <p className="mb-3">
+            You have {family.children.length} children:{' '}
+            {family.children.map((child) => (
+              <span key={child.firstName}>
+                {child.firstName} {child.familyName},{' '}
+              </span>
+            ))}
+          </p>
+        </>
+      )}
     </main>
   );
 }
