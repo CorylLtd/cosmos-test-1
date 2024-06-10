490ae406622830fa1d423f8096a9a264531e2a07
Martin Randall <martin@coryl.com>
2024-06-10 15:24:57 +0100

    image test

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import { getFamily } from '@/actions/db';
+import Image from 'next/image';
 
 export const dynamic = 'force-dynamic';
 
 export default async function Home() {
   const family = await getFamily('Randall');
+  const imageUrl = `https://${process.env.IMAGE_STORE}/photos/thumbnail.jpg`;
 
   return (
     <main className="p-4">
+      <Image src={imageUrl} alt="test image" width={500} height={200}></Image>
       <h1 className="text-3xl font-bold mb-4">Home</h1>
       {family && (
         <>
